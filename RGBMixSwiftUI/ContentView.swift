fcbf1f6d82499ff61885a99fd9482f5cdf98f507
Vladimir Izmaylov <holybuddha@yandex.ru>
2021-10-30 21:46:31 +0300

    Initial Commit

---
+++ RGBMixSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBMixSwiftUI
+//
+//  Created by Vladimir Izmaylov on 30.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RGBMixSwiftUI/RGBMixSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBMixSwiftUIApp.swift
+//  RGBMixSwiftUI
+//
+//  Created by Vladimir Izmaylov on 30.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBMixSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
